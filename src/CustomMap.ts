6b7e861f42d04e4131df34acf0aac1f8de65db12
amitzed <amitzmn@gmail.com>
2020-12-24 16:42:06 -0500

    When marker clicked, content is displayed

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -19,13 +19,21 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
     });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'Testing'
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    })
   }
 
 }
