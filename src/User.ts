f00e7003275ea46bb11f485f1932ff48b6c5132b
amitzed <amitzmn@gmail.com>
2020-12-23 20:28:27 -0500

    imported company and user classes into index.ts

--- src/User.ts
+++ src/User.ts
@@ -1,7 +1,17 @@
-class User {
+import * as faker from 'faker';
+
+export class User {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
 }
